92686e7b269d48318d102632c7ad781174e69590
Voloshin_Sergey <4voloshin@gmail.com>
2021-03-08 11:14:47 +0300

    feat: add 22 day task

---
+++ 22_day Follow Along Links/README.md
@@ -0,0 +1,7 @@
+# 22 - Follow Along Nav
+
+**Challenge:** When you hover your mouse over any link, the link is highlighted in its width and height.
+
+**Things to Learn:** Using getBoundingClientRect method, properties scrollY and scrollX, to calculate the position and size of the highlighted area.
+
+[DEMO](https://voloshin-sergei.github.io/30DaysOfJavaScript/22_day%20Follow%20Along%20Nav/)
---
+++ 22_day Follow Along Links/scripts.js
@@ -0,0 +1,21 @@
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highLightLink() {
+  linkCoords = this.getBoundingClientRect();
+  //console.log(linkCoords);
+  const cords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX,
+  };
+
+  highlight.style.width = `${cords.width}px`;
+  highlight.style.height = `${cords.height}px`;
+  highlight.style.transform = `translate(${cords.left}px, ${cords.top}px)`;
+}
+
+triggers.forEach((a) => a.addEventListener('mouseenter', highLightLink));
