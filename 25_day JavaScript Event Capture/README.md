f6ec9c6605807bcf6238fff9f6541f8809c5163f
Voloshin_Sergey <4voloshin@gmail.com>
2021-03-11 21:31:47 +0300

    feat: add 25 day task

---
+++ 25_day JavaScript Event Capture/README.md
@@ -0,0 +1,9 @@
+# 25 - JavaScript Event Capture
+
+**Challenge:** Learn about event capturing.
+
+**Things to Learn:** bubbling, propagation, property called once.
+
+![demo](demo.png)
+
+[DEMO](https://voloshin-sergei.github.io/30DaysOfJavaScript/25_day%20JavaScript%20Event%20Capture/)
---
+++ 25_day JavaScript Event Capture/scripts.js
@@ -0,0 +1,20 @@
+const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
+
+function logText(e) {
+  console.log(this.classList.value);
+  e.stopPropagation(); // stop bubbling
+}
+
+//document.body.addEventListener('click', logText);
+divs.forEach((div) => div.addEventListener('click', logText));
+
+button.addEventListener(
+  'click',
+  () => {
+    console.log('Click!');
+  },
+  {
+    once: true,
+  },
+);
